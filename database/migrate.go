347b42946386e803575d70bebafbcfd979b61f55
Mathias Magnusson <mathias@magnusson.space>
2023-09-27 22:59:11 +0200

    Make migration fail if there are migrations in the database but not in the directory

--- database/migrate.go
+++ database/migrate.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"embed"
+	"errors"
 	"log/slog"
 	"sort"
 )
@@ -27,42 +28,71 @@ func Migrate(db *sql.DB, ctx context.Context) error {
 	sort.Slice(entries, func(i, j int) bool {
 		return entries[i].Name() < entries[j].Name()
 	})
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	res, err := tx.Query(`
+		SELECT name
+		FROM __migrations
+	`)
+	if err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+	for res.Next() {
+		var applied string
+		res.Scan(&applied)
+		found := false
+		for _, entry := range entries {
+			if entry.Name() == applied {
+				found = true
+				break
+			}
+		}
+		if !found {
+			_ = tx.Rollback()
+			return errors.New("Applied migration " + applied + " not found in file system")
+		}
+	}
+	if err != nil {
+		_ = tx.Rollback()
+		return err
+	}
 	for _, entry := range entries {
 		migration, err := migrations.ReadFile("migrations/" + entry.Name())
 		if err != nil {
+			_ = tx.Rollback()
 			return err
 		}
 
-		tx, err := db.BeginTx(ctx, nil)
-		if err != nil {
-			return err
-		}
-
-		if err := tx.QueryRowContext(ctx, `
+		if err := tx.QueryRow(`
 			SELECT FROM __migrations WHERE name = $1
 		`, entry.Name()).Scan(); err == nil {
 			// No error means we found the migration, meaning it's already applied.
 			continue
 		} else if err != sql.ErrNoRows {
+			_ = tx.Rollback()
 			return err
 		}
 
 		slog.InfoContext(ctx, "Applying migration", "name", entry.Name())
 
-		if _, err := tx.ExecContext(ctx, string(migration)); err != nil {
-			tx.Rollback()
+		if _, err := tx.Exec(string(migration)); err != nil {
+			_ = tx.Rollback()
 			return err
 		}
-		if _, err := tx.ExecContext(ctx, `
+		if _, err := tx.Exec(`
 			INSERT INTO __migrations (name) VALUES ($1)
 		`, entry.Name()); err != nil {
-			tx.Rollback()
+			_ = tx.Rollback()
 			return err
 		}
 
-		if err := tx.Commit(); err != nil {
-			return err
-		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
 	}
 	return nil
 }
